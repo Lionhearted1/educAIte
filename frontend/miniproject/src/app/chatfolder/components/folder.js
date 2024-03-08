d35dd48fa82f005c657af1c9bbd052502d2fa591
varrun-v <varunsvmg2@gmail.com>
2024-03-08 20:09:52 +0530

    chatfolder

--- frontend/miniproject/src/app/chatfolder/components/folder.js
+++ frontend/miniproject/src/app/chatfolder/components/folder.js
@@ -1,10 +1,10 @@
 "use client";
 import styles from "./style.css";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import Card1Component from "./Card1Component";
 import axios from "axios";
-import { useRouter } from 'next/navigation'
-import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 const Folder = () => {
   const [showOverlay, setShowOverlay] = useState(false);
@@ -16,36 +16,38 @@ const Folder = () => {
   const fetchFoldersFromExternalApi = async () => {
     try {
       const apiUrl = `http://127.0.0.1:3000/folders/get_all`;
-      const user_name = 'hello'; // Replace this with your actual user_name
+      const user_name = "hello"; // Replace this with your actual user_name
       const response = await axios.get(apiUrl, { params: { user_name } });
-      const response_folders= response.data.folders;
-      setFolders(response_folders)
+      const response_folders = response.data.folders;
+      setFolders(response_folders);
       console.log(response.data);
     } catch (error) {
-      console.error('Error fetching folders:', error);
+      console.error("Error fetching folders:", error);
     }
   };
 
   // Function to save folders to the external API
   const saveFoldersToExternalApi = async () => {
     try {
-      
-      const response = await axios.post('http://127.0.0.1:3000/folders/create', {
-        unique_id: uuidv4(),
-        folder_name: fileName,
-        user_name: "hello",
-      });
-  
+      const response = await axios.post(
+        "http://127.0.0.1:3000/folders/create",
+        {
+          unique_id: uuidv4(),
+          folder_name: fileName,
+          user_name: "hello",
+        }
+      );
+
       if (response.status === 200) {
-        console.log('Folder saved to external API successfully');
+        console.log("Folder saved to external API successfully");
       } else {
-        console.error('Unexpected response from external API:', response);
+        console.error("Unexpected response from external API:", response);
       }
     } catch (error) {
-      console.error('Error saving folder to external API:', error);
+      console.error("Error saving folder to external API:", error);
     }
   };
-  
+
   const handleAddFolder = () => {
     setShowOverlay(true);
   };
@@ -54,7 +56,6 @@ const Folder = () => {
     saveFoldersToExternalApi();
     setShowOverlay(false);
     window.location.reload();
-    
   };
 
   // Fetch folders on component mount
@@ -62,65 +63,63 @@ const Folder = () => {
     fetchFoldersFromExternalApi();
   }, []);
 
-    return (
-      <div>
-        <center>
-          <div
-            className="bg-white w-[90vw] h-[80vh]  rounded-lg border-2 mt-[05%]
+  return (
+    <div>
+      <center>
+        <div
+          className="bg-white w-[90vw] h-[75vh]  rounded-lg border-2
                        border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] px-20 py-12 overflow-y-auto"
-          >
-            {/* <div
-              className="bg-yellow-400 h-16 absolute w-16 rounded-full border-0 top-8 left-[50%]  -rotate-12  border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] over"
-            ></div> */}
-            <center>
-              <h1 className="text-2xl font-bold mb-4 ">Chat Folders</h1>
-            </center>
-            <div className="grid grid-cols-3 gap-20 h-full">
-              {folders.map((folder) => (
-                <Card1Component key={folder.id} folder={folder} />
-              ))}
-            </div>
-            
-            {showOverlay && (
-              <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                <div className="bg-white w-[40vw] h-[40vh] p-4 rounded">
+        >
+          <div className="bg-yellow-400 h-16 absolute w-16 rounded-full border-0 top-8 left-[10%] mt-4 -rotate-12  border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] over"></div>
+          <center>
+            <h1 className="text-2xl font-bold mb-4 ">Chat Folders</h1>
+          </center>
+          <div className="grid grid-cols-3 gap-20 h-full">
+            {folders.map((folder) => (
+              <Card1Component key={folder.id} folder={folder} />
+            ))}
+          </div>
+
+          {showOverlay && (
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+              <div className="bg-white w-[40vw] h-[40vh] p-4 rounded">
+                <center>
+                  <h2 className="mb-2 text-4xl font-extrabold font-mono mt-5">
+                    Add Folder
+                  </h2>
+                </center>
+                <div className="mx-20 mb-20 mt-12">
                   <center>
-                    <h2 className="mb-2 text-4xl font-extrabold font-mono mt-5">
-                      Add Folder
-                    </h2>
+                    <input
+                      type="text"
+                      value={fileName}
+                      onChange={(e) => setFileName(e.target.value)}
+                      className="mb-2 mx-4 p-2 border-2 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] rounded-lg hover:shadow-none hover:translate-x-1 hover:translate-y-1 duration-150"
+                      placeholder="Enter file name"
+                    />
+                    <button
+                      className="bg-purple-500 border-2 mt-5 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] hover:bg-purple-200 text-white py-2 hover:shadow-none hover:translate-x-1 hover:translate-y-1 duration-150 px-4 rounded-lg"
+                      onClick={handleSave}
+                    >
+                      Save
+                    </button>
                   </center>
-                  <div className="mx-20 mb-20 mt-12">
-                    <center>
-                      <input
-                        type="text"
-                        value={fileName}
-                        onChange={(e) => setFileName(e.target.value)}
-                        className="mb-2 mx-4 p-2 border-2 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] rounded-lg hover:shadow-none hover:translate-x-1 hover:translate-y-1 duration-150"
-                        placeholder="Enter file name"
-                      />
-                      <button
-                        className="bg-purple-500 border-2 mt-5 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)] hover:bg-purple-200 text-white py-2 hover:shadow-none hover:translate-x-1 hover:translate-y-1 duration-150 px-4 rounded-lg"
-                        onClick={handleSave}
-                      >
-                        Save
-                      </button>
-                    </center>
-                  </div>
                 </div>
               </div>
-            )}
-          </div>
-          <center>
-              <button
-                className="bg-yellow-500 hover:bg-yellow-200 text-black border-black border-2 py-2 px-4 rounded-full hover:shadow-none hover:translate-x-1 hover:translate-y-1 duration-150 mt-4 shadow-[8px_8px_0px_rgba(0,0,0,1)]"
-                onClick={handleAddFolder}
-              >
-                Add Folder
-              </button>
-            </center>
+            </div>
+          )}
+        </div>
+        <center>
+          <button
+            className="bg-yellow-500 hover:bg-yellow-200 text-black border-black border-2 py-2 px-4 rounded-full hover:shadow-none hover:translate-x-1 hover:translate-y-1 duration-150 mt-4 shadow-[8px_8px_0px_rgba(0,0,0,1)]"
+            onClick={handleAddFolder}
+          >
+            Add Folder
+          </button>
         </center>
-      </div>
-    );
-  };
+      </center>
+    </div>
+  );
+};
 
 export default Folder;
--- frontend/miniproject/src/app/chatfolder/page.js
+++ frontend/miniproject/src/app/chatfolder/page.js
@@ -1,15 +1,19 @@
 "use client";
 import NavSelectComponent from "../dashboard/components/NavSelectComponent";
 import Folder from "./components/folder";
+import Link from "next/link";
+
 
 function ChatFolder() {
   return (
     <div className="flex flex-col">
       <div className="flex justify-between p-4">
         <div>
+          <Link href="/dashboard">
           <button className="bg-purple-600 hover:bg-purple-400 shadow-[8px_8px_0px_rgba(0,0,0,1)] border-2 border-black text-black font-bold py-2 px-4 rounded-lg focus:outline-none hover:shadow-none hover:translate-x-1 hover:translate-y-1 duration-150">
             Back
           </button>
+          </Link>
         </div>
         <div>
           <NavSelectComponent items={["Home", "Chat", "Logout"]} />
